7ba1fa3ffc8dcdebdee8f86ade99f88c8bfcbe02
LisandroHdz <99113543+LisandroHdz@users.noreply.github.com>
2024-12-11 17:48:20 -0600

    ARCHIVOS1.0
    ARCHIVOS1.0

---
+++ jdh/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu-icon').addEventListener('click', function() {
+    const nav = document.querySelector('nav ul');
+    nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
+});
